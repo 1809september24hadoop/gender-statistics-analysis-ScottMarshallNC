b760103a26a4c0acc2e0657eadc1a74a9572474a
ScottMarshallnc <cloudera@quickstart.cloudera>
2018-10-23 11:14:33 -0700

    3,4 working order

---
+++ Project1/src/main/java/com/revature/PercentFemaleEmployement.java
@@ -0,0 +1,38 @@
+package com.revature;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import com.revature.map.GenderStatMapper;
+import com.revature.reduce.PercentFemaleEmployementByCountryReducer;
+
+public class PercentFemaleEmployement {
+
+	public static void main(String[] args) throws Exception {
+		if(args.length != 2){
+			System.out.printf("Usage: TestCheck <input dir> <output dir>\n");
+			System.exit(-1);
+		}
+		
+		Job job = new Job();
+		
+		job.setJarByClass(GenderStatMapper.class);
+		job.setJobName("Test 3");
+		
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.setMapperClass(GenderStatMapper.class);
+		job.setReducerClass(PercentFemaleEmployementByCountryReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		boolean success = job.waitForCompletion(true);
+		System.exit(success ? 0 : 1);
+	}
+}
